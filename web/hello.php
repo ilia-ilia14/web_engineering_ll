83accee0462975f1c3d84e03233d79aaa48003c6
ilia-ilia14 <simon.ilia.sd@gmail.com>
2018-04-29 22:01:37 -0600

    added Hello world page

---
+++ web/hello.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<title>"Hello World" Application</title>
+	<style>
+		body {
+			background-color: #2D2D2D;
+		}
+		
+		h1 {
+			color: #C26356;
+			font-size: 30px;
+			font-family: Menlo, Monaco, fixed-width;
+			text-align: center;
+			margin-top:15%;
+		}
+		
+		p {
+			color: white;
+			font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1><?php
+		echo "Hello World";
+?></h1>
+	<p>Made by Ilia Sidamonidze.</p>
+</body>
+</html>
+
