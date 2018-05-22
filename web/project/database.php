c3ea05b583f5dfe34bc63ed8ee041e8fc6620898
ilia-ilia14 <simon.ilia.sd@gmail.com>
2018-05-22 15:34:26 -0600

    project database commit1

--- web/project/database.php
+++ web/project/database.php
@@ -1,17 +1,20 @@
 <?php
-$dbUrl = getenv('postgres://kmvcasrhbaqbpb:9ad6033621581f9819aa3ab7a526842f63bf3a5f16b02fd950a80d827be47af0@ec2-54-235-132-202.compute-1.amazonaws.com:5432/d6pvfb79n04ha4');
+$dbUrl = getenv('DATABASE_URL');
 
 $dbopts = parse_url($dbUrl);
 
-$dbHost = $dbopts["ec2-54-235-132-202.compute-1.amazonaws.com"];
-$dbPort = $dbopts["5432"];
-$dbUser = $dbopts["kmvcasrhbaqbpb"];
-$dbPassword = $dbopts["9ad6033621581f9819aa3ab7a526842f63bf3a5f16b02fd950a80d827be47af0"];
-$dbName = ltrim($dbopts["d6pvfb79n04ha4"],'/');
-    try {       
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+
+    try {  
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+
 		echo "trying";
     } catch (PDOException $e) {
 
