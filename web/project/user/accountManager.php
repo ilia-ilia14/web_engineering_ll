96595b6ffb870e334ed6ef25891478043baf5f22
ilia-ilia14 <simon.ilia.sd@gmail.com>
2018-06-04 01:05:55 -0600

    account

--- web/project/user/accountManager.php
+++ web/project/user/accountManager.php
@@ -4,65 +4,95 @@ if(!isset($_SESSION))
 	session_start();
 }
 ?>
-<DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
-<head> 
-<title> Account Manager </title>
-<link style="text/css" rel="stylesheet" href="../css/indv-styles.css" />
-<link rel="stylesheet" type="text/css" href="../css/product.css">
-<link rel="stylesheet" type="text/css" href="../css/reg-style.css">
-<link rel="stylesheet" type="text/css" href="../css/productview.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/product_style.css">
+   <link style="text/css" rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
-<?php include 'view/header.php'; ?>
+<style>
+.centered-form{
+	margin-top: 60px;
+}
 
-<div id="reg-div" >
+.centered-form .panel{
+	background: rgba(255, 255, 255, 0.8);
+	box-shadow: rgba(0, 0, 0, 0.3) 20px 20px 20px;
+}
+</style>
+<script type="text/javascript">
+	$(document).ready(function(){
+  var placeholder = null;
+  $('input[type=text]').focus(function(){
+    placeholder = $(this).attr("placeholder");
+    $(this).attr("placeholder","");
+  });
+  $('input[type=text]').blur(function(){
+    $(this).attr("placeholder", placeholder);
+  });
+});
+</script>
+    <body>
+<?php include ("components/navigation.php"); ?>
+       <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Edit Account </h3>
+			 			</div>
+			 			<div class="panel-body">
+						<form id="reg-form" method="post" action="index.php">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			                <input type="text" name="firstname" id="name1"  class="form-control input-sm" value="<?php echo $member['firstName']; ?>" />
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" id="name1" name="lastname"  class="form-control input-sm" value="<?php echo $member['lastName']; ?>" required />
+			    					</div>
+			    				</div>
+			    			</div>
 
+			    			<div class="form-group">
+			    				<input type="text" id="email" name="email"  class="form-control input-sm" value="<?php echo $member['Email']; ?>" required />
+			    			</div>
 
-
-        <div class="register">
-          <h3> Edit Account</h3>
-          <b style="color:red;"> <?php echo $error; ?> </b>
-          <form id="reg-form" method="post" action="index.php">
-            <div>
-              <label for="name">Firstname*</label>
-              <input type="text" name="firstname" id="name1" value="<?php echo $member['firstName']; ?>" />
-            </div>
-            <div>
-              <label for="name">Lastname*</label>
-              <input type="text" id="name1" name="lastname" value="<?php echo $member['lastName']; ?>" required/>
-            </div>
-            <div>
-              <label for="email">Email*</label>
-              <input type="text" id="email" name="email" value="<?php echo $member['Email']; ?>" required/>
-            </div>
-            <div>
-              <label for="email">Phone*</label>
-              <input type="number" name="phone" value="<?php echo $member['Phone']; ?>" required/>
-            </div>
-            <input type="hidden" name="member_id" value="<?php echo $member['memberID']; ?>" />
-            <div>
-              <label for="password">Password*</label>
-              <input type="password" id="password" name="password" required/>
-            </div>
-            <div>
-              <label for="password-again1">Password Again*</label>
-              <input type="password" id="password-again" name="password1" required/>
-            </div>
-            <div>
-             <br />
-              <input type="submit" value="Save" name="action"/>
-            </div>
-          </form>
-        </div>
-
-</div>
-
-
-<?php include 'view/navbar.php'; ?>
- <!-- ################################################################################################ -->
-<?php include 'view/footer.php'; ?>
-</body>
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" id="password" name="password" required class="form-control input-sm" placeholder="Password" />
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" id="password-again" name="password1" required class="form-control input-sm" placeholder="Confirm Password" />
+			    					</div>
+			    				</div>
+			    			</div>
+			    			
+			    			<input type="submit" value="Save" name="action" class="btn btn-info btn-block">
+			    		
+			    		</form>
+						<b style="color:red;"> 
+							<?php  if(!empty($error)) {
+							foreach ( $error as $err ) : ?>
+						   <?php echo $err."<br>"; ?>
+							<?php endforeach; }?>
+						</b>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>      
+    </body>
+	
+</script>
 </html>
